03681cf51d57e3b8b99e28e498bdfe6152611940
João Vitor Castilho <joaovicastilho0@gmail.com>
2022-11-08 17:51:36 -0300

    ex 6

---
+++ src/main/java/app/ex6.java
@@ -0,0 +1,27 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ex6 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        List<Integer> valores = new ArrayList<>();
+        System.out.print("Insira o primeiro numero: ");
+        Integer a = input.nextInt();
+        System.out.print("Insira o segundo numero: ");
+        Integer b = input.nextInt();
+        valores.add(a, b);
+
+        boolean result = false;
+
+        if (result.compareTo(a, b) == false){
+
+        }
+
+        input.close();
+    }
+}
